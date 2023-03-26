ed6d61cba0b2293cd4a07b18cfb226b250cb79b9
a-mikhaylov <sasha.mikhailov99.com@gmail.com>
2023-03-26 22:14:03 +0300

    Написал первый тест, но пока что надо прибраться

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,18 +1,21 @@
 package com.example.demo;
 
+import com.pageobject.demo.Main.MainPage;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.concurrent.TimeUnit;
 
 public class Test {
     public static void main(String[] args) {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
+        chromeOptions.addArguments("--remote-allow-origins=*", "ignore-certificate-errors");
         ChromeDriver driver = new ChromeDriver(chromeOptions);
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://crtweb.ru/");
+        driver.get("https://github.com/login");
+        MainPage mainPage = new MainPage(driver);
+        mainPage.login("", "");
         driver.quit();
     }
 }
